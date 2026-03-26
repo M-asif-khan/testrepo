156717700a8d960f80589ec795da795c5be7082b
asif <asifsharif9298@gmail.com>
2026-03-26 08:14:01 +0500

    add home

---
+++ lib/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder(
+      child: Center(
+        child: Container(
+          color: Colors.blue,
+          child: Center(
+            child: Text('this is home', style: TextStyle(color: Colors.red)),
+          ),
+        ),
+      ),
+    );
+  }
+}
